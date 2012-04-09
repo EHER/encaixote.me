32d343f021a7aa98e3d2c17266159204e9d220b1
Eher <alexandre@eher.com.br>
2012-04-08 23:12:07 -0300

    Organização do JS e tapa no layout

--- facebook.js
+++ facebook.js
@@ -1,51 +1,7 @@
-var checkFacebookLogin = function() {
-        FB.getLoginStatus(function(response) {
-            if (response.status === 'connected') {
-                $("#login").hide();
-                $("#logout").show();
-                $("#gallery").show();
-                // the user is logged in and has authenticated your
-                // app, and response.authResponse supplies
-                // the user's ID, a valid access token, a signed
-                // request, and the time the access token 
-                // and signed request each expire
-                var uid = response.authResponse.userID;
-                var accessToken = response.authResponse.accessToken;
-            } else if (response.status === 'not_authorized') {
-                // the user is logged in to Facebook, 
-                // but has not authenticated your app
-                $("#login").show();
-                $("#logout").hide();
-                $("#gallery").hide();
-            } else {
-                // the user isn't logged in to Facebook.
-                $("#login").show();
-                $("#logout").hide();
-                $("#gallery").hide();
-            }
-        });
-};
-
-var addPhoto = function(id, url) {
-    photo  = '<div id="pic-" class="pic" style="top:px;left:px;background:url('+url+') no-repeat 50% 50%; -moz-transform:rotate(deg); -webkit-transform:rotate(deg);">';
-    photo += '<a class="fancybox" rel="fncbx" href="'+url+'" target="_blank"></a>';
-    photo += '</div>';
-    $("#gallery").append(photo);
-};
-
-var getPhotos = function() {
-    FB.api('me/home/photos', function(response) {
-        $(response.data).each(function(index, data) {
-            console.log(data);
-            addPhoto(data.id, data.picture);
-        });
-    });
-};
-
 window.fbAsyncInit = function() {
     FB.init({
-        appId      : '340748665982158', // App ID
-        channelUrl : '//localhost/src/facebox/channel.php', // Channel File
+        appId      : '359200690783172', // App ID
+        channelUrl : '//encaixote.me/channel.php', // Channel File
         status     : true, // check login status
         cookie     : true, // enable cookies to allow the server to access the session
         xfbml      : true  // parse XFBML
--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+var checkFacebookLogin = function() {
+        FB.getLoginStatus(function(response) {
+            if (response.status === 'connected') {
+                $("#login").hide();
+                $("#logout").show();
+                $("#gallery").show();
+                // the user is logged in and has authenticated your
+                // app, and response.authResponse supplies
+                // the user's ID, a valid access token, a signed
+                // request, and the time the access token 
+                // and signed request each expire
+                var uid = response.authResponse.userID;
+                var accessToken = response.authResponse.accessToken;
+                getPhotos();
+            } else if (response.status === 'not_authorized') {
+                // the user is logged in to Facebook, 
+                // but has not authenticated your app
+                $("#login").show();
+                $("#logout").hide();
+                $("#gallery").hide();
+            } else {
+                // the user isn't logged in to Facebook.
+                $("#login").show();
+                $("#logout").hide();
+                $("#gallery").hide();
+            }
+        });
+};
+
+var addPhoto = function(id, url) {
+    if(id !== undefined && url !== undefined) {
+        url_small = url;
+        url_big = url.replace("_s.jpg", "_b.jpg");
+
+        position_top = Math.floor(Math.random()*100)*3;
+        position_left = Math.floor(Math.random()*100)*8;
+        position_rotate = Math.floor(Math.random()*20)-10;
+
+        photo  = '<div id="pic-'+id+'" class="pic" style="top:'+position_top+'px;left:'+position_left+'px;background:url('+url_small+') no-repeat 50% 50%; -moz-transform:rotate('+position_rotate+'deg); -webkit-transform:rotate('+position_rotate+'deg);">';
+        photo += '<a class="fancybox" rel="fncbx" href="'+url_big+'" target="_blank"></a>';
+        photo += '</div>';
+        $("#gallery").append(photo);
+    }
+};
+
+var getPhotos = function() {
+    FB.api('me/home/photos', function(response) {
+        $(response.data).each(function(index, data) {
+            console.log(data);
+            addPhoto(data.id, data.picture);
+        });
+        magica();
+    });
+};
+
 $(document).ready(function(){
     // Executed once all the page elements are loaded
     $("#login").bind("click",function(e){
